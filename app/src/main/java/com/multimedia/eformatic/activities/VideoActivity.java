f8d937e4e1ae954677e0bac959409c7fc342b388
Sandra <snaboulet@modulotech.fr>
2015-09-07 10:32:40 +0200

    Landscape + fullscreen mode for video

--- app/src/main/java/com/multimedia/eformatic/activities/VideoActivity.java
+++ app/src/main/java/com/multimedia/eformatic/activities/VideoActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -22,8 +24,7 @@ public class VideoActivity extends ActionBarActivity {
     private Item mItem;
 
     private VideoView mVideoView;
-    private TextView mTitle;
-    private TextView mDuration;
+
 
     private int position = 0;
     private ProgressDialog progressDialog;
@@ -32,7 +33,12 @@ public class VideoActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
+
 
         Intent i = getIntent();
         Bundle extras = i.getExtras();
@@ -44,28 +50,16 @@ public class VideoActivity extends ActionBarActivity {
         mTraining = TrainingManager.getInstance().getTraining(ean);
         mItem = TrainingManager.getInstance().getItem(ean, itemId);
 
+
         setContentView(R.layout.activity_video);
 
         if (mTraining != null) {
             getSupportActionBar().setTitle(mTraining.getTitle());
         }
 
-
         mVideoView = (VideoView) findViewById(R.id.video_view);
-        mTitle = (TextView) findViewById(R.id.video_title);
-        mDuration = (TextView) findViewById(R.id.video_duration);
-
-        if (mItem != null) {
-            mTitle.setText(mItem.getTitle());
-            mDuration.setText(mItem.getDuration());
-        }
-
-        // Media
-
-
 
     }
-
 
 
     @Override
@@ -76,7 +70,7 @@ public class VideoActivity extends ActionBarActivity {
             mediaControls.setMinimumHeight(200);
         }
 
-        Uri uri= Uri.parse(mItem.getVideo().getFilepath());
+        Uri uri = Uri.parse(mItem.getVideo().getFilepath());
         mVideoView.setMediaController(mediaControls);
         mVideoView.setVideoURI(uri);
         mVideoView.start();
